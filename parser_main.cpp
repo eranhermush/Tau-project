b9ab6698928677a607db034fe089f9c901b8e32a
Eran Hermush <eranhermush@localhost.localdomain>
2019-04-08 19:24:22 +0300

    update parser_main

--- parser_main.cpp
+++ parser_main.cpp
@@ -6,12 +6,14 @@
 
 parser_main::parser_main(str)
 {
-    str_regular_expression = str;
+    this->str_regular_expression = str;
+    this->param_size = str.length();
+    this->matrix_all_options = vector< vector<string> >(this->param_size);
 }
 
-Vector<char> & parser_main::from_known_char_to_options_list(char c)
+vector<char> parser_main::from_known_char_to_options_list(char c)
 {
-    char result[] = {};
+    vector<char> result = {};
     switch(c)
     {
         case 'c' :
@@ -24,5 +26,14 @@ Vector<char> & parser_main::from_known_char_to_options_list(char c)
             result = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
             break;
     }
-    return vector<char> (myints, myints + sizeof(myints) / sizeof(int) );
+    return result;
+}
+
+void parser_main::from_parser_string_to_matrix(){
+    for (int i = 0; i< this->param_size; i++){
+        this->matrix_all_options.at(i) = this->from_known_char_to_options_list(this->str_regular_expression.at(i));
+    }
+}
+vector<vector<string>> parser_main::get_matrix{
+    return this->matrix_all_options;
 }
--- parser_main.h
+++ parser_main.h
@@ -4,7 +4,9 @@
 
 #ifndef TAU_PROJECT_PARSER_MAIN_H
 #define TAU_PROJECT_PARSER_MAIN_H
-
+#include <iostream>
+#include <string>
+using namespace std;
 /**
  * This class is the main for the parser
  * it has functions that convert the regular expression string to array of tokens
@@ -14,18 +16,34 @@ using namespace std;
 
 class parser_main {
     string str_regular_expression;
+    int param_size;
+    vector< vector<string> > matrix_all_options;
+
 public:
     /**
      * constructor
      * @param str- the regular expression string
      */
     parser_main(string str);
+
     /**
      * convert regular expression char to its array of values
      * @param c: the known char of the regular expression, like 'c'
      * @return  The array of option for this char, or empty vector for unknown char
      */
-    Vector<char>& from_known_char_to_options_list(char c);
+    vector<char> from_known_char_to_options_list(char c);  
+
+    /**
+        convert the parser string to the parser matrix
+        assusme that the parser_main object is intialized
+    */     
+    void from_parser_string_to_matrix();    
+    /*
+    getter for the matrix
+    we assume that the matrix is intialized
+    */
+    <vector<vector<string>> get_matrix();
+
 
 };
 
