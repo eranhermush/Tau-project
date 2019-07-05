5c0b7df01f3a04343f0c34a824f71f5707328e56
Eran Hermush <eranhermush@localhost.localdomain>
2019-07-05 12:36:02 +0300

    finish first part - index

--- file_manager.cpp
+++ file_manager.cpp
@@ -5,6 +5,7 @@ file_manager::file_manager(std::vector< std::vector<std::string> > &matrix_all_o
     this->matrix_all_options = matrix_all_options;
     this->file_names = file_names;
     this->scheme_string = scheme_string;
+    this->file_char = 'f';
     validate_input();
 }
 
@@ -12,9 +13,9 @@ int file_manager::vector_indexes_to_index(std::vector<int> &vec)
 {
     int result = 0;
     int sum = 1;
-    for (int i = 0; i<vec.size() ;i++){
+    for (int i = 0; i<vec.size();i++){
         result += sum*vec[i];
-        sum *= this->matrix_all_options[i].size();
+        sum *= size_of_object_in_scheme(i);
     }
     return result;
 }
@@ -29,13 +30,55 @@ bool file_manager::validate_input()
     {
         throw std::invalid_argument("received different sizes for the matrix and the string");
     }
-    if(std::count(this->scheme_string.begin(), this->scheme_string.end(), 'f') != this->file_names.size())
+    if(std::count(this->scheme_string.begin(), this->scheme_string.end(), this->file_char) != this->file_names.size())
     {
         throw std::invalid_argument("received different sizes for the file names and the number of files in the scheme");
     }
     return true;
 }
 
-int file_manager::size_of_object_in_scheme(int index){
+int file_manager::size_of_object_in_scheme(int index)
+{
+    if(index <0 || index >= this->scheme_string.length()){
+        throw std::invalid_argument("unexpected index");
+    }
+    if(this->scheme_string.at(index) != file_char){
+        return this->matrix_all_options[index].size();
+    }
+    int file_index = 0;
+    for (int i = 0; i <= index; ++i)
+    {
+        if (this->scheme_string.at(i) == this->file_char)
+        {
+            file_index++;
+        }
+    }
+    return Get_number_of_lines_in_file(this->file_names[file_index]);
+}
+int file_manager::Get_number_of_lines_in_file(std::string filename)
+{
+    int number_of_lines = 0;
+    std::string line;
+    std::ifstream myfile("textexample.txt");
 
+    while (std::getline(myfile, line))
+        ++number_of_lines;
+    return number_of_lines;
+}
+
+std::vector<int>& file_manager::index_to_vector_indexes(int index){
+    vector<int> result; 
+    // fill the array with 0 this->scheme_string.length() times 
+    result.assign(this->scheme_string.length(), 0); 
+    int sum = 1;
+    for (int i = 0; i<this->scheme_string.length();i++){
+        sum *= size_of_object_in_scheme(i);
+    }
+
+    for (int i = this->scheme_string.length()-1; i >= 0; i--)
+    {
+        result[i] = (int) index/sum;
+        sum = sum / size_of_object_in_scheme(i);
+    }
+    return result;
 }
--- file_manager.h
+++ file_manager.h
@@ -19,17 +19,18 @@ class file_manager{
     std::vector< std::vector<std::string> > matrix_all_options;
     std::vector<std::string> file_names;
     std::string scheme_string;
+    char file_char; // char of a file in the string scheme
 
 
 
 public:
-	/**
+	/*
 	 * constructor
 	 * @param matrix_all_options- the matrix from the parser with all the options
      * @param file_names        - the names of our files (with repetitions) that we use in the scheme
 	 */
     file_manager(std::vector< std::vector<std::string> > &matrix_all_options, std::vector<std::string> &file_names, std::string &scheme_string);
-    /**
+    /*
         This function validates that the input is valid:
             The size of the string == the size of the matrix
             There is a file name for each file option in the scheme
@@ -41,12 +42,18 @@ public:
     //get_new_work();
     //bool generate_new_work_to_file();
     //bool check_file_need_to_update();
-    std::vector<int>& index_to_vector_indexes(int vector);
+    std::vector<int>& index_to_vector_indexes(int index);
     int vector_indexes_to_index(std::vector<int> &vec);
     /*
     * This function gets an index in the scheme and returns the size of the object that this letter represents. e.g. the size of the file.
     */
     int size_of_object_in_scheme(int index);
+
+    /*
+        This function returns how many lines are in a file
+    */
+    int Get_number_of_lines_in_file(std::string filename);
+
 };
 
 
