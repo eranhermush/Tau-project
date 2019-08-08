ec38e8ee435b30e630c04f122b45f570cad3c501
Oded <odedr1@mail.tau.ac.il>
2019-08-08 21:13:23 +0300

    changed indexing. constructor now takes starting and ending point

--- nested_password_generator.cpp
+++ nested_password_generator.cpp
@@ -1,34 +1,132 @@
 #include "nested_password_generator.h"
 
-Nested_Password_Generator::Nested_Password_Generator(std::vector<std::unique_ptr<Password_Generator>>& generators, bool should_reset){
-	gen_amount = generators.size();
-	pass_generators.reserve(gen_amount);
-	for(int i = 0; i < gen_amount; ++i){
-		pass_generators.push_back(generators[i]->clone());
-		if(should_reset){
-			pass_generators[i]->reset();
-		}
-	}
+Nested_Password_Generator::Nested_Password_Generator(const std::vector<std::unique_ptr<Password_Generator>>& generators,
+	const std::vector<uint64_t>& start_pos, const std::vector<uint64_t>& end_pos){
 
-	init();
+	gen_amount = std::min(generators.size(), std::min(start_pos.size(), end_pos.size()));
+	init_generators(generators);
+
+	curr_pos_vector.reserve(gen_amount);
+
+	init_vectors(start_pos, end_pos);
+
+	curr_pos_vector = start_pos_vector;
+
+	total_start_pos = vector_to_number(start_pos_vector, generator_lengths);
+	total_end_pos = vector_to_number(start_pos_vector, generator_lengths);
+
+
+	init_password();
+}
+
+Nested_Password_Generator::Nested_Password_Generator(const std::vector<std::unique_ptr<Password_Generator>>& generators,
+	uint64_t total_start, uint64_t total_end){
+
+	gen_amount = generators.size();
+	init_generators(generators);
+
+	curr_pos_vector.reserve(gen_amount);
+	
+	std::vector<uint64_t> sp(gen_amount, 0);
+	std::vector<uint64_t> ep(gen_amount, 0);
+	number_to_vector(total_start, generator_lengths, sp);
+	number_to_vector(total_end, generator_lengths, ep);
+
+	init_vectors(sp, ep);
+
+	curr_pos_vector = start_pos_vector;
+
+	total_start_pos = vector_to_number(start_pos_vector, generator_lengths);
+	total_end_pos = vector_to_number(start_pos_vector, generator_lengths);
+
+
+	init_password();
 }
 
 Nested_Password_Generator::Nested_Password_Generator(const Nested_Password_Generator& generator)
-	:gen_amount(generator.gen_amount), pass_ranges(generator.pass_ranges), has_next_flag(generator.has_next_flag){
-		curr_password = generator.curr_password;
-		is_over_flag = generator.is_over_flag;
-		pass_generators.reserve(gen_amount);
-		for(int i = 0; i < gen_amount; ++i){
-			pass_generators.push_back(generator.pass_generators[i]->clone());
+	:gen_amount(generator.gen_amount){
+		init_generators(generator.pass_generators);
+		init_vectors(generator.start_pos_vector, generator.end_pos_vector);
+		if(generator.curr_pos_vector < start_pos_vector){
+			curr_pos_vector = start_pos_vector;
 		}
+		else if(generator.curr_pos_vector > end_pos_vector){
+			curr_pos_vector = end_pos_vector;
+		}
+		else{
+			curr_pos_vector = generator.curr_pos_vector;
+		}
+		has_next_flag = (curr_pos_vector < end_pos_vector);
+		init_password();
 }
 
 std::unique_ptr<Password_Generator>  Nested_Password_Generator::clone() const{
 	return std::unique_ptr<Password_Generator>(new Nested_Password_Generator(*this));
 }
 
+uint64_t Nested_Password_Generator::vector_to_number(const std::vector<uint64_t>& digits, const std::vector<uint64_t>& bases){
+	int length = std::min(bases.size(), digits.size());
+	uint64_t result = 0;
+	uint64_t prev_base = 1;
+	for(int i = 0; i < length; ++i){
+		result = result * prev_base + digits[i];
+		prev_base = bases[i];
+	}
+	return result;
+}
 
-void Nested_Password_Generator::init(){
+void Nested_Password_Generator::number_to_vector(uint64_t number, const std::vector<uint64_t>& bases, std::vector<uint64_t>& dest){
+	int length = std::min(bases.size(), dest.size());
+	for(int i = length - 1; i >= 0; --i){
+		dest[i] = number % bases[i];
+		number /= bases[i];
+	}
+}
+
+void Nested_Password_Generator::init_generators(const std::vector<std::unique_ptr<Password_Generator>>& generators){
+	pass_generators.reserve(gen_amount);
+	generator_lengths.reserve(gen_amount);
+
+	for(int i = 0; i < gen_amount; ++i){
+		pass_generators.push_back(generators[i]->clone());
+		pass_generators[i]->expand_to_max_range();
+		generator_lengths.push_back(pass_generators[i]->get_range_length());
+	}
+}
+
+void Nested_Password_Generator::init_vectors(const std::vector<uint64_t>& start_pos, const std::vector<uint64_t>& end_pos){
+	
+	end_pos_vector.reserve(gen_amount);
+	start_pos_vector.reserve(gen_amount);
+
+	for(int i = 0; i < gen_amount; ++i){
+		start_pos_vector.push_back(std::min(start_pos[i], generator_lengths[i] - 1));
+		end_pos_vector.push_back(std::min(end_pos[i], generator_lengths[i] - 1));
+	}
+	// vector comare is lexicographic compare
+	if(start_pos_vector > end_pos_vector){
+		end_pos_vector = start_pos_vector;
+	}
+}
+
+void Nested_Password_Generator::set_password(const std::vector<uint64_t>& new_pos){
+	if(new_pos < start_pos_vector || new_pos > end_pos_vector){
+		return;
+	}
+	curr_pos_vector = new_pos;
+	init_password();
+	is_over_flag = false;
+	if(!has_next_flag && curr_pos_vector < end_pos_vector){
+		has_next_flag = true;
+	}
+}
+
+
+void Nested_Password_Generator::init_password(){
+	//init the generators
+	for(int i = 0; i < gen_amount; ++i){
+		pass_generators[i]->set_password(curr_pos_vector[i]);
+	}
 	int total_length = 0;
 	int temp_length = 0;
 	std::string temp_password;
@@ -66,24 +164,28 @@ void Nested_Password_Generator::advance_password(){
 		if(pass_generators[index]->has_next()){
 			// advance this coordinate
 			pass_generators[index]->advance_password();
+			curr_pos_vector[index] = pass_generators[index]->get_curr_position();
 			break;
 		}
 		// carry the change back
 		else{
 			pass_generators[index]->reset();
+			curr_pos_vector[index] = pass_generators[index]->get_curr_position();
 		}
+	}
+	// if we 'carried' from the first generator, there is no next at all
+	// this might happen if the first generator only had the one password
+	if(index < 0){
+		has_next_flag = false;
+		is_over_flag = true;
+		return;
 	}
 	// change the password
 	update_password_suffix(index);
 
-	// update the has next flag, if needed
-	for(index = gen_amount - 1; index >= 0; --index){
-		if(pass_generators[index]->has_next()){
-			// if any sub-generator has another password, the nested generator has another
-			return;
-		}
-	}
-	has_next_flag = false;
+	// update the has next flag, if we reached the last vector there is no next in the range
+	// may not be accurate when it comes to actual end files, that's why there are more tests
+	has_next_flag = (curr_pos_vector < end_pos_vector);
 }
 
 void Nested_Password_Generator::update_password_suffix(int beginning_index){
@@ -92,7 +194,7 @@ void Nested_Password_Generator::update_password_suffix(int beginning_index){
 	int i = beginning_index < gen_amount ? beginning_index : gen_amount; 
 	for(; i < gen_amount; ++i){
 		sub_password = pass_generators[i]->get_password();
-		if(pass_generators[i]->has_fixed_length() || sub_password.length() == (pass_ranges[i+1] - pass_ranges[i])){
+		if(pass_generators[i]->has_fixed_length() || sub_password.length() == (unsigned int)(pass_ranges[i+1] - pass_ranges[i])){
 			curr_password.replace(pass_ranges[i], pass_ranges[i+1] - pass_ranges[i], sub_password);
 		}
 		else{
@@ -114,10 +216,25 @@ void Nested_Password_Generator::update_password_suffix(int beginning_index){
 }
 
 void Nested_Password_Generator::reset(){
-	// zero all generators
-	for(int i = 0; i < gen_amount; ++i){
-		pass_generators[i]->reset();
-	}
+	// zero all coordinates
+	curr_pos_vector = start_pos_vector;
 	// build the passwords
-	init();
+	init_password();
+}
+
+void Nested_Password_Generator::expand_to_max_range(){
+	uint64_t total_size = 1;
+	for(int i = 0; i < gen_amount; ++i){
+		start_pos_vector[i] = 0;
+		end_pos_vector[i] = generator_lengths[i] - 1;
+		total_size *= generator_lengths[i];
+	}
+	if(!has_next_flag && curr_pos_vector < end_pos_vector){
+		has_next_flag = true;
+	}
+	if(is_over_flag && curr_pos_vector != end_pos_vector){
+		is_over_flag = false;
+	}
+	total_start_pos = 0;
+	total_end_pos = total_size - 1;
 }
--- nested_password_generator.h
+++ nested_password_generator.h
@@ -3,12 +3,23 @@
 
 #include <vector>
 #include "password_generator.h"
+#include <cstdint>
+#include <algorithm>
 
-#include <iostream>
+/* A password generator that concatenates a few simpler generators
+	Has a higher complexity cost than simpler generator,
+	Therefore avoid using if possible, and avoid a doubly nested generator */
 
 class Nested_Password_Generator: public Password_Generator{
 	public:
-		Nested_Password_Generator(std::vector<std::unique_ptr<Password_Generator>>& generators, bool should_reset=true);
+		/* Constuctors a nested generator with in the given range */
+		Nested_Password_Generator(const std::vector<std::unique_ptr<Password_Generator>>& generators,
+			const std::vector<uint64_t>& start_pos, const std::vector<uint64_t>& end_pos);
+
+		/* Constuctors a nested generator with in the given range, where total_start and total_end
+			are converted to vectors */
+		Nested_Password_Generator(const std::vector<std::unique_ptr<Password_Generator>>& generators,
+			uint64_t total_start, uint64_t total_end);
 
 		/* Copy Constructor - copies the current state as well*/
 		Nested_Password_Generator(const Nested_Password_Generator& generator);
@@ -18,6 +29,15 @@ class Nested_Password_Generator: public Password_Generator{
 
 		/* Advances the current password, if possible */
 		void advance_password();
+
+		/* Sets the current password to be the one at position (position should be in the range) */
+		void set_password(const std::vector<uint64_t>& new_pos);
+
+		void set_password(uint64_t position){
+			std::vector<uint64_t> new_pos(gen_amount, 0);
+			number_to_vector(position, generator_lengths, new_pos);
+			set_password(new_pos);
+		}
 
 		/* Returns whether the generator has another password to produce */
 		bool has_next() const{
@@ -32,13 +52,40 @@ class Nested_Password_Generator: public Password_Generator{
 			return false;
 		}
 
+		/* Returns the length of the current range
+			Avoid using unless you are sure why, the units are instance dependent */
+		uint64_t get_range_length() const{
+			return total_end_pos - total_start_pos + 1;
+		}
+
+		void expand_to_max_range();
+
+		/* Returns the current position in the range (from 0 *not* from reset)
+			Avoid using unless you are sure why, high complexity cost and not much use*/
+		uint64_t get_curr_position() const{
+			return vector_to_number(generator_lengths, curr_pos_vector);
+		}
+
 	private:
 		int gen_amount; /* how many generators are used */
 		std::vector<std::unique_ptr<Password_Generator>> pass_generators;
 		std::vector<int> pass_ranges; /* The first index of each subpassword */
+		std::vector<uint64_t> generator_lengths;
+		std::vector<uint64_t> curr_pos_vector;
+		std::vector<uint64_t> end_pos_vector;
+		std::vector<uint64_t> start_pos_vector;
+		uint64_t total_start_pos;
+		uint64_t total_end_pos;
 		bool has_next_flag = false;
 
-		void init();
+		static uint64_t vector_to_number(const std::vector<uint64_t>& digits, const std::vector<uint64_t>& bases);
+		static void number_to_vector(uint64_t number, const std::vector<uint64_t>& bases, std::vector<uint64_t>& dest); // puts number representation in dest
+																												// dest and basis must be of the same size
+		
+		void init_generators(const std::vector<std::unique_ptr<Password_Generator>>& generators); // assumes gen_amount is already correct
+		void init_vectors(const std::vector<uint64_t>& start_pos, const std::vector<uint64_t>& end_pos); // assumes gen_amount, pass_generators,
+																										// generator_lengths are already correct
+		void init_password();
 		void update_password_suffix(int beginning_index);
 };
 
